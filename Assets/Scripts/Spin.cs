6827314619c18db63883bbdac76ed70dc52dad58
Bearg84 <pushkinda@gmail.com>
2024-07-02 08:40:41 +0300

    version 2

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -5,18 +5,9 @@ using UnityEngine;
 public class Spin : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Transform _rotationCenter;
-    [SerializeField] private Vector3 _offset;
-
-    private void Start()
-    {
-        _offset = transform.position - _rotationCenter.position;
-    }
 
     private void Update()
     {
-        transform.position = _rotationCenter.position + _offset;
         transform.Rotate(Vector3.up * _speed * Time.deltaTime);
-        _offset = transform.position - _rotationCenter.position;
     }
 }
